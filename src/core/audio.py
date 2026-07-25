32e9151a35c7755d56c5ac42ba0b1c0ad9066fb4
F1SH3R <salvatorescalese03@gmail.com>
2026-07-25 02:08:02 +0200

    Fix audio backend using pygame and ALSA HDMI output

--- src/core/audio.py
+++ src/core/audio.py
@@ -1,5 +1,6 @@
 import os
-import subprocess
+os.environ["SDL_AUDIODRIVER"] = "alsa"
+import pygame
 
 
 class AudioManager:
@@ -7,26 +8,61 @@ class AudioManager:
     def __init__(self):
 
         self.path = "assets/sounds"
+        os.environ["ALSA_PCM_CARD"] = "1"
+        print("AUDIO INIT:", pygame.mixer.get_init())
+        pygame.mixer.init(
+            frequency=44100,
+            size=-16,
+            channels=2,
+            buffer=512
+        )
+
+        self.sounds = {}
+
+        self.load_sounds()
+
+
+    def load_sounds(self):
+
+        for filename in os.listdir(self.path):
+
+            if filename.endswith(".wav"):
+
+                path = os.path.join(
+                    self.path,
+                    filename
+                )
+
+                try:
+
+                    self.sounds[filename] = pygame.mixer.Sound(path)
+
+                    print(
+                        f"Loaded sound: {filename}"
+                    )
+
+                except Exception as e:
+
+                    print(
+                        f"AUDIO LOAD ERROR {filename}: {e}"
+                    )
 
 
     def play(self, filename):
 
         try:
 
-            sound_path = os.path.join(
-                self.path,
-                filename
-            )
+            sound = self.sounds.get(filename)
 
-            subprocess.Popen(
-                [
-                    "aplay",
-                    "-q",
-                    sound_path
-                ],
-                stdout=subprocess.DEVNULL,
-                stderr=subprocess.DEVNULL
-            )
+            if sound:
+
+                sound.play()
+
+            else:
+
+                print(
+                    f"SOUND NOT FOUND: {filename}"
+                )
 
 
         except Exception as e:
